fe33f23061762f9255c6b77042c1d5f9f6a145f1
JoshuaMinne <61147892+JoshuaMinne@users.noreply.github.com>
2020-03-21 17:49:41 +0100

    Indienen opdracht

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/9a85ba95-4a23-4f20-5d6b-3261b04e1acb-ecb4/",
                 "target": "_blank"
             }
         ]
